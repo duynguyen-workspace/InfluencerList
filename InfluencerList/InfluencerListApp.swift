51c3d838e0f274d8aba699873dbe3be0c0509669
duynguyen-workspace <duynguyen.workspace@gmail.com>
2023-07-25 22:43:35 +0700

    Initial Commit

---
+++ InfluencerList/InfluencerListApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfluencerListApp.swift
+//  InfluencerList
+//
+//  Created by Nguyễn Anh Duy on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct InfluencerListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
