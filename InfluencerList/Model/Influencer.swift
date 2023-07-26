81cdf67c3bc30ee3540734b3dfe03732e089a39e
duynguyen-workspace <duynguyen.workspace@gmail.com>
2023-07-26 12:57:12 +0700

    Update Influencer Model to contain map attributes and model data

--- InfluencerList/Model/Influencer.swift
+++ InfluencerList/Model/Influencer.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import CoreLocation
 
 struct Influencer: Identifiable, Codable{
     var id: Int
@@ -19,4 +20,18 @@ struct Influencer: Identifiable, Codable{
     var image: Image {
         Image(imageName)
     }
+    
+    var coordinates: Coordinates
+        
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+}
+
+struct Coordinates: Codable {
+    var latitude: Double
+    var longitude: Double
 }
--- InfluencerList/Model/ModelData.swift
+++ InfluencerList/Model/ModelData.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreLocation
 
 var influencers = decodeJsonFromJsonFile(jsonFileName: "influencers.json")
 
